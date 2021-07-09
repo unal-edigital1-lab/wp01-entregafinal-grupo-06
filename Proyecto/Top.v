0d5b1c4e0e54173ddf468871837e3f993c7ec9db
jcaipap <jcaipap@unal.edu.co>
2021-07-09 11:50:16 -0500

    Pruebas de antirrebote

--- Proyecto/Top.v
+++ Proyecto/Top.v
@@ -15,10 +15,24 @@ module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,
 	output [0:6] sseg;
 	output [3:0] an;
 
+	
+wire[3:0] filaAntirrebote;
 wire [3:0] posT;
 wire [3:0] posVGA;
 wire opr;
 wire [3:0] datOutR;
+	
+	
+	
+	
+
+
+antirrebote fila4(.clk(clk),.rst(rst),.ButtonIn(fila[3]),.ButtonOut(filaAntirrebote[3]));
+antirrebote fila3(.clk(clk),.rst(rst),.ButtonIn(fila[2]),.ButtonOut(filaAntirrebote[2]));
+antirrebote fila2(.clk(clk),.rst(rst),.ButtonIn(fila[1]),.ButtonOut(filaAntirrebote[1]));
+antirrebote fila1(.clk(clk),.rst(rst),.ButtonIn(fila[0]),.ButtonOut(filaAntirrebote[0]));
+	
+	
 	
 Teclado teclado(
 .clk(clk),
@@ -51,4 +65,4 @@ test_VGA VGA(
 
 Display display(.clk(clk),.numA(datOutR),.sseg(sseg),.an(an));
 	
-endmodule	
+endmodule
