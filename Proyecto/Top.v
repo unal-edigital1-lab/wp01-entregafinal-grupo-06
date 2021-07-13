7403aae1874cd141be120f1af0f9fe9937f94f07
jcaipap <jcaipap@unal.edu.co>
2021-07-13 14:30:25 -0500

    Prueba vga con switches

--- Proyecto/Top.v
+++ Proyecto/Top.v
@@ -22,7 +22,7 @@ wire[3:0] filaAntirrebote;
 wire [3:0] posT;
 reg[3:0]RegPrueba;
 wire [3:0] posVGA;
-wire opr;
+//wire opr;
 wire [3:0] datOutR;
 wire [3:0] posDispay;	
 	
@@ -47,7 +47,7 @@ Teclado teclado(
 	
 BancoRegistro #( 4,3,"C:/Users/equip/Documents/GitHub/wp01-testvga-grupo-6/Proyecto/memDir.men")banco(
 .addrR(posVGA),
-.addrW(posT),
+.addrW(prueba),
 .RegWrite(pruebaOPR),
 .clk(clk),
 .rst(~rst),
