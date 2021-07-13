79f14c0100a9d7b92e2bee28cc6b226fa8bf7d16
aholguinr <aholguinr@unal.edu.co>
2021-07-13 14:03:56 -0500

    cambios quitar teclado
    quitar el codigo de teclado y usar dp para verificar que funcione correctamente el display

--- Proyecto/Top.v
+++ Proyecto/Top.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 
-module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,an);
+module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,an,prueba,pruebaOPR);
 	input clk;
 	input rst;
 	output [3:0]col;
@@ -15,16 +15,16 @@ module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,
 	output [0:6] sseg;
 	output [3:0] an;
 
+	input[3:0]prueba;
+	input pruebaOPR;
 	
 wire[3:0] filaAntirrebote;
 wire [3:0] posT;
+reg[3:0]RegPrueba;
 wire [3:0] posVGA;
 wire opr;
 wire [3:0] datOutR;
 wire [3:0] posDispay;	
-	
-	
-
 	
 //antirrebote fila4(.clk(clk),.ButtonIn(fila[3]),.ButtonOut(filaAntirrebote[3]));
 //antirrebote fila3(.clk(clk),.ButtonIn(fila[2]),.ButtonOut(filaAntirrebote[2]));
@@ -32,6 +32,10 @@ wire [3:0] posDispay;
 //antirrebote fila1(.clk(clk),.ButtonIn(fila[0]),.ButtonOut(filaAntirrebote[0]));
 	
 	
+always@(*)begin
+	RegPrueba=prueba;
+end
+
 	
 Teclado teclado(
 .clk(clk),
@@ -44,7 +48,7 @@ Teclado teclado(
 BancoRegistro #( 4,3,"C:/Users/equip/Documents/GitHub/wp01-testvga-grupo-6/Proyecto/memDir.men")banco(
 .addrR(posVGA),
 .addrW(posT),
-.RegWrite(opr),
+.RegWrite(pruebaOPR),
 .clk(clk),
 .rst(~rst),
 .datOutR(datOutR)
