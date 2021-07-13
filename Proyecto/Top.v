4e74e8c6c4e496e197456bf3fc2cf565581fcc49
jcaipap <jcaipap@unal.edu.co>
2021-07-13 15:10:27 -0500

    Correcto funcionamiento del antirrebote con switches

--- Proyecto/Top.v
+++ Proyecto/Top.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 
-module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,an,prueba,pruebaOPR);
+module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,prueba,pruebaOPR);
 	input clk;
 	input rst;
 	output [3:0]col;
@@ -12,8 +12,8 @@ module Top(clk,rst,col,fila, opr,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,
 	output wire VGA_R;	
 	output wire VGA_G;  
 	output wire VGA_B;
-	output [0:6] sseg;
-	output [3:0] an;
+	//output [0:6] sseg;
+	//output [3:0] an;
 
 	input[3:0]prueba;
 	input pruebaOPR;
@@ -26,14 +26,19 @@ wire [3:0] posVGA;
 wire [3:0] datOutR;
 wire [3:0] posDispay;	
 	
-//antirrebote fila4(.clk(clk),.ButtonIn(fila[3]),.ButtonOut(filaAntirrebote[3]));
-//antirrebote fila3(.clk(clk),.ButtonIn(fila[2]),.ButtonOut(filaAntirrebote[2]));
-//antirrebote fila2(.clk(clk),.ButtonIn(fila[1]),.ButtonOut(filaAntirrebote[1]));
-//antirrebote fila1(.clk(clk),.ButtonIn(fila[0]),.ButtonOut(filaAntirrebote[0]));
-	
+wire pruebaOPRAntiR;
+wire [3:0] pruebaAntiR;
+
 always@(*)begin
 	RegPrueba=prueba;
 end
+
+antirrebote bloqueOPR(.clk(clk),.ButtonIn(pruebaOPR),.ButtonOut(pruebaOPRAntiR));
+antirrebote fila3(.clk(clk),.ButtonIn(prueba[3]),.ButtonOut(pruebaAntiR[3]));
+antirrebote fila2(.clk(clk),.ButtonIn(prueba[2]),.ButtonOut(pruebaAntiR[2]));
+antirrebote fila1(.clk(clk),.ButtonIn(prueba[1]),.ButtonOut(pruebaAntiR[1]));
+antirrebote fila0(.clk(clk),.ButtonIn(prueba[0]),.ButtonOut(pruebaAntiR[0]));
+	
 
 	
 Teclado teclado(
@@ -45,9 +50,9 @@ Teclado teclado(
 );
 	
 BancoRegistro #( 4,3,"C:/Users/equip/Documents/GitHub/wp01-testvga-grupo-6/Proyecto/memDir.men")banco(
-.addrR(prueba),
-.addrW(prueba),
-.RegWrite(pruebaOPR),
+.addrR(posVGA),
+.addrW(pruebaAntiR),
+.RegWrite(pruebaOPRAntiR),
 .clk(clk),
 .rst(~rst),
 .datOutR(datOutR)
@@ -67,8 +72,7 @@ test_VGA VGA(
 
 
 
-Display display(.clk(clk),.numA(datOutR),.sseg(sseg),.an(an));
-	
+//Display display(.clk(clk),.numA(datOutR),.sseg(sseg),.an(an));
 	
 
-endmodule 
+endmodule
