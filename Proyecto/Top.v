3555c59348ae5497691d80796dbcf50fa6015b96
aholguinr <aholguinr@unal.edu.co>
2021-08-02 11:10:28 -0500

    cambios top

--- Proyecto/Top.v
+++ Proyecto/Top.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 
-module Top(clk,rst,clkPWM,col,fila,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sseg,an,bell,prueba,pruebaOPR,pin);
+module Top(clk,rst,clkPWM,col,fila,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,bell,prueba,pruebaOPR,pin);
 	input clk;
 	input rst;
 	input clkPWM;
@@ -12,15 +12,13 @@ module Top(clk,rst,clkPWM,col,fila,VGA_Hsync_n,VGA_Vsync_n,VGA_R,VGA_G,VGA_B,sse
 	output wire VGA_R;	
 	output wire VGA_G;  
 	output wire VGA_B;
-	output [0:6] sseg;
-	output [3:0] an;
 	output bell;
 	output pin;
 
 	input[3:0]prueba;
 	input pruebaOPR;
 	
-wire[3:0] filaAntirrebote;
+wire [3:0] filaAntirrebote;
 wire [3:0] posT;
 reg[3:0]RegPrueba;
 wire [3:0] posVGA;
@@ -28,15 +26,17 @@ wire opr;
 wire oprAR;
 wire [3:0] datOutR;
 wire [3:0] posDispay;	
-	
+reg [11:0] Nfreq;
 wire pruebaOPRAntiR;
 wire [3:0] pruebaAntiR;
-
-wire [7:0] numCaso;
+wire salidaPWM;
 
 reg regPrueba=1'b1;
 
 assign pin=regPrueba;
+assign bell=(salidaPWM & opr);
+
+
 
 /*
 always@(*)begin
@@ -84,11 +84,11 @@ test_VGA VGA(
 	.VGA_B(VGA_B),   	
 );
 
-
-pwm_basico#(14,15)(.clk(clk),.reset(rst),.pwm_out(bell),.caso(numCaso));
-
-
-Display display(.clk(clk),.numA(numCaso),.sseg(sseg),.an(an));
+always @(*) begin
+Nfreq=1000+50*posT;
+	end
 	
+pwm_basico#(6)(.clk(clk),.Nentrada(Nfreq),.pwm_out(salidaPWM));
 
-endmodule
+
+endmodule 
--- Proyecto/pwm_basico.v
+++ Proyecto/pwm_basico.v
@@ -1,100 +1,49 @@
 `timescale 1ns/1ps
 
 module pwm_basico
-	#(parameter R = 8, parameter N=5)(
-	
-
+	#(parameter R = 6)(
 	input clk,
-	input reset,
-	//output reg [R - 1:0] ciclo,
-	output pwm_out,
-	output reg [5:0]caso
-	//output reg [R-1:0] Q_reg
+	input [11:0] Nentrada,
+	output pwm_out
 	);
 	
+	reg [R - 1:0] Q_reg=0;
+	reg [R - 1:0] ciclo=0;
+	reg [5:0]caso =0;
+	reg[11:0] N;
+	reg [11:0] n=0;
+	assign N=Nentrada;
 	
-	//Contador de flanco positivo
-	reg [R - 1:0] Q_reg;
-	reg [R - 1:0] ciclo;
-	//reg [5:0]caso =0;
-	initial begin caso =0;
-	end
+	assign pwm_out = (Q_reg <ciclo);	
 	
-	/* Divisor de frecuecia
-	wire enable;
-	reg [26:0] cfreq=0;
-	assign enable = cfreq[18];
-	always @(posedge clk) begin
-			cfreq<=cfreq+1;
-	end*/
-
 	always @(posedge clk)
 	begin
 		Q_reg <= Q_reg+1;
 		
 	end
 	
-	always @(posedge clk)
-	begin
-		if(Q_reg==2**(R*N-1)) begin
-			if(caso>=35) caso=0;
-			else if(caso<35)caso=caso+1;
-		end
-	end
-
 	always @(*) begin
-	/* case(caso) 
-			0: ciclo=128; 
-			1: ciclo=150;
-			2: ciclo=172;
-			3: ciclo=193; 
-			4: ciclo=211;
-			5: ciclo=227;
-			6: ciclo=240;
-			7: ciclo=249;
-			8: ciclo=254;
-			9: ciclo=255;
-			10: ciclo=252;
-			11: ciclo=245;
-			12: ciclo=234;
-			13: ciclo=220;
-			14: ciclo=202;
-			15: ciclo=183;
-			16: ciclo=161;
-			17: ciclo=139;
-			18: ciclo=116;
-			19: ciclo=94;
-			20: ciclo=72;
-			21: ciclo=53;
-			22: ciclo=35;
-			23: ciclo=21;
-			24: ciclo=10;
-			25: ciclo=3;
-			26: ciclo=0;
-			27: ciclo=1;
-			28: ciclo=6;
-			29: ciclo=15;
-			30: ciclo=28;
-			31: ciclo=44;
-			32: ciclo=62;
-			33: ciclo=83;
-			34: ciclo=105;
-			35: ciclo=128;
-			default: ciclo=0;
-			
-		endcase*/
+	if(Q_reg==2**R-1) begin 
+	n=n+1;
+	end
+	
+	if(n==N)begin
+		if(caso>=35) caso=0;
+		else caso=caso+1;	
+		n=0;end
+	
 	case(caso)
-			0: ciclo=2**R*0.5; 
-			1: ciclo=2**R*0.5893;  
-			2: ciclo=2**R*0.6757;  
-			3: ciclo=2**R*0.7564;  
-			4: ciclo=2**R*0.829;  
-			5: ciclo=2**R*0.8909;  
-			6: ciclo=2**R*0.9403;  
-			7: ciclo=2**R*0.9755; 
-			8: ciclo=2**R*0.9955;
-			9: ciclo=2**R*0.9995;
-			10: ciclo=2**R*0.9875;
+			0: ciclo=2**R*0.5; // 
+			1: ciclo=2**R*0.5893; // 
+			2: ciclo=2**R*0.6757; // 
+			3: ciclo=2**R*0.7564; // 
+			4: ciclo=2**R*0.829; // 
+			5: ciclo=2**R*0.8909; // 
+			6: ciclo=2**R*0.9403; // 
+			7: ciclo=2**R*0.9755; // 
+			8: ciclo=2**R-1;
+			9: ciclo=2**R-1;
+			10: ciclo=2**R-1;
 			11: ciclo=2**R*0.9598;
 			12: ciclo=2**R*0.9173;
 			13: ciclo=2**R*0.8614;
@@ -120,12 +69,10 @@ module pwm_basico
 			33: ciclo=2**R*0.3243;
 			34: ciclo=2**R*0.4107;
 			35: ciclo=2**R*0.5;
-			default:     ciclo=0; 
+			default:     ciclo=2**R-1; // 
 		endcase
+		
 	end
-	
-	assign pwm_out = (Q_reg <ciclo);
-	
+
 	
 endmodule	
-	
